69611c42d359092f70195073332472f814a25dc7
bobmatyas <ballium@protonmail.com>
2019-06-08 10:50:00 -0500

    initial project skeleton

---
+++ components/photos/photos.js
@@ -0,0 +1,16 @@
+function PhotosController(PhotoService, $q) {
+  var ctrl = this;
+
+ 
+
+
+}
+
+angular.module('ColorApp').component('photos', {
+  template: `
+      <section id="quote">
+        <h2>Photos</h2>
+      </section>
+    `, // or use templateUrl
+  controller: PhotosController
+});
