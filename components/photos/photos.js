4cfcc4543dd482a69017938eab87aad7de7cfb5d
Eric Medema <eamedema@gmail.com>
2019-06-12 18:15:35 -0400

    fixing my merge issues

--- components/photos/photos.js
+++ components/photos/photos.js
@@ -1,27 +1,67 @@
 function PhotosController(PhotoService, $q) {
   var ctrl = this;
 
-  // retrieve photos on initial page load
+  // retrieve photos on initial page load -- mainly for testing
 
   ctrl.$onInit = function() {
-    ctrl.photos = [];
-    ctrl.getPhotos();
+
+    // ctrl.getPhotos();
     ctrl.indPhoto = '';
     ctrl.getIndividualPhoto(4252039);
-    ctrl.imageTest = 'https://pixabay.com/get/52e2d0414a51a514f6da8c7dda79367c123ddee352576c4870297bd49644c65fb0_1280.jpg';
-    ctrl.getColorPalette(ctrl.imageTest);
   };
 
-  // this function gets random photos
 
-  ctrl.getPhotos = () => {
+  /** 
+   * A static array of categories for the search.
+   * The category list is provided by Pixabay. 
+  */
+
+  ctrl.photoCategories = [
+    'animals', 
+    'backgrounds', 
+    'buildings', 
+    'business', 
+    'computer', 
+    'education', 
+    'fashion', 
+    'feelings', 
+    'food', 
+    'health', 
+    'industry', 
+    'music', 
+    'nature', 
+    'people', 
+    'places', 
+    'religion', 
+    'science', 
+    'sports', 
+    'transportation', 
+    'travel'
+  ]; 
+
+
+  /** 
+   * A static array of photo orientations.
+   * The orientation list is provided by Pixabay. 
+  */
+
+  ctrl.photoOrientations = [
+    'all',
+    'horizontal',
+    'vertical'
+  ];
+
+
+  // this function gets the photos based on user search criteria
+
+  ctrl.getPhotos = (queryText, photoCategory, photoOrientation) => {
 
     return $q(function(resolve, reject) {
-      PhotoService.getPhotos()      
+      PhotoService.getPhotos(queryText, photoCategory, photoOrientation)      
         .then( (response) => {
           console.log(response.data.hits);  
           ctrl.photos = response.data.hits;
-          // console.log(ctrl.photos);
+          console.log(ctrl.photos);
           resolve();
           }
         ) 
@@ -52,22 +92,7 @@ function PhotosController(PhotoService, $q) {
     });
   }
 
-  ctrl.getColorPalette = (imageTest) => {
-    return $q(function(resolve, reject) {
-      PhotoService.getColorPalette(imageTest)
-        .then( (response) => {
-          console.log(`color scheme`);
-          ctrl.colorResponse = response.data;
-          console.log(ctrl.colorResponse);
-          resolve();
-          }
-        )
-        .catch( function(error) {
-          console.error(error);
-          throw error;
-        });
-    });
-  }
+  
 
   ctrl.imageColor = () => {
       PhotoService.extractColor()
@@ -90,26 +115,32 @@ function PhotosController(PhotoService, $q) {
 angular.module('ColorApp').component('photos', {
   template: `
       <section id="photos">
+        <h2>Search</h2>
+        <input type="text" maxlength="100" ng-model="$ctrl.photoSearch" placeholder="what type of image?" ng-keypress="($event.charCode==13)? $ctrl.getPhotos($ctrl.photoSearch, $ctrl.photoCategory, $ctrl.photoOrientation) : return" />
+        <select ng-model="$ctrl.photoCategory">
+          <option value="" disabled selected hidden>Please Choose... </option>
+          <option value=""></option>
+          <option ng-repeat="category in $ctrl.photoCategories" value="{{ category }}"> {{ category.charAt(0).toUpperCase()+ category.substr(1).toLowerCase()  }}</option>
+        </select>
+        <select ng-model="$ctrl.photoOrientation"> 
+          <option ng-repeat="orientation in $ctrl.photoOrientations" value="{{ orientation }}"> {{ orientation.charAt(0).toUpperCase()+ orientation.substr(1).toLowerCase()  }}</option>
+        </select>
+        <button class="button-green" ng-click="$ctrl.getPhotos($ctrl.photoSearch, $ctrl.photoCategory, $ctrl.photoOrientation)">
+          Search
+        </button>
         <h2>Photos</h2>
-
         <h2>List of Photos from Pixabay API</h2>
-
-        <div ng-repeat="photo in $ctrl.photos">
+        <div ng-if="$ctrl.photos.length >= 1" ng-repeat="photo in $ctrl.photos">
           <img src="{{ photo.previewURL }}" />
         </div>
-
-        <h2>Individual Photo from Pixabay API</h2>
-
-        <div ng-repeat="indPhoto in $ctrl.indPhoto">
-          <img src="{{ indPhoto.largeImageURL }}" />
+        <div ng-if="$ctrl.photos.length < 1">
+          <p style="color: red; font-weight: bold;">No results.</p>
         </div>
-
-        <h2>Color Scheme Test</h2>
-
+        <h2>TEST: Individual Photo from Pixabay API</h2>
+        <div ng-repeat="indPhoto in $ctrl.indPhoto">
+          <img src="{{ indPhoto.largeImageURL }}" style="max-width: 100%;"/>
+        </div>
         <button ng-click="$ctrl.imageColor()">Test Button</button>
-
-
-
       </section>
     `, // or use templateUrl
   controller: PhotosController
