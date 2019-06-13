6d14b21184d06ea7d03b3baf5273341b9e4764ed
bobmatyas <ballium@protonmail.com>
2019-06-12 21:09:10 -0500

    home component working

--- components/home/home.js
+++ components/home/home.js
@@ -1,5 +1,23 @@
 function HomeController() {
+
   var ctrl = this;
+
+  // these come direct from the Pixabay API
+
+  ctrl.colors = ["grayscale", "transparent", "red", "orange", "yellow", "green", "turquoise", "blue", "lilac", "pink", "white", "gray", "black", "brown"];
+
+  ctrl.selectedColorTrueFalse = true; 
+
+
+  ctrl.selectColor = (color) => {
+    console.log(ctrl.selectedColorTrueFalse);
+    console.log('clicked');
+    ctrl.selectedColorTrueFalse = true;
+    ctrl.selectedColor = color;
+    console.log(ctrl.selectedColor);
+    console.log(ctrl.selectedColorTrueFalse);
+  }
+
 
 }
 
@@ -10,20 +28,17 @@ angular.module('ColorApp').component('home', {
       <h1>What color photos are you looking for?</h1>
     </div>
   
-    <div class="color-grid">
-        <a href="#"><div class="color" id="red"></div></a>      
-        <a href="#"><div class="color" id="orange"></div></a>
-        <a href="#"><div class="color" id="yellow"></div></a>
-        <a href="#"><div class="color" id="green"></div></a>
-        <a href="#"><div class="color" id="turquoise"></div></a>
-        <a href="#"><div class="color" id="blue"></div></a>
-        <a href="#"><div class="color" id="lilac"></div></a>
-        <a href="#"><div class="color" id="pink"></div></a>
-        <a href="#"><div class="color" id="white"></div></a>
-        <a href="#"><div class="color" id="black"></div></a>
-        <a href="#"><div class="color" id="grey"></div></a>
-        <a href="#"><div class="color" id="grayscale"></div></a>
+      <div class="color-grid">
+        <a href="#" ng-repeat="color in $ctrl.colors" ng-click="$ctrl.selectColor(color)"> 
+          <div class="color" id="{{ color }}"></div>
+        </a>      
       </div>
+
+      <p>Color: {{ ctrl.selectedColor }}</p>
+      <div class="home__search__bar" ng-if="$ctrl.selectedColorTrueFalse === false"> 
+        <input type="text" ng-model="$ctrl.search">
+      </div>
+
       </section>
     `, // or use templateUrl
   controller: HomeController
