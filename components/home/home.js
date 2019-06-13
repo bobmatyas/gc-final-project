54a90a897e740cf22c8d2e52e5b822303bfeb0c4
bobmatyas <ballium@protonmail.com>
2019-06-12 19:40:07 -0500

    made home component and fixed over 100% width error

--- components/home/home.js
+++ components/home/home.js
@@ -6,7 +6,24 @@ function HomeController() {
 angular.module('ColorApp').component('home', {
   template: `
       <section id="home">
-        <h2>Home</h2>
+      <div class="home-text">
+      <h1>What color photos are you looking for?</h1>
+    </div>
+  
+    <div class="color-grid">
+        <a href="#"><div class="color" id="red"></div></a>      
+        <a href="#"><div class="color" id="orange"></div></a>
+        <a href="#"><div class="color" id="yellow"></div></a>
+        <a href="#"><div class="color" id="green"></div></a>
+        <a href="#"><div class="color" id="turquoise"></div></a>
+        <a href="#"><div class="color" id="blue"></div></a>
+        <a href="#"><div class="color" id="lilac"></div></a>
+        <a href="#"><div class="color" id="pink"></div></a>
+        <a href="#"><div class="color" id="white"></div></a>
+        <a href="#"><div class="color" id="black"></div></a>
+        <a href="#"><div class="color" id="grey"></div></a>
+        <a href="#"><div class="color" id="grayscale"></div></a>
+      </div>
       </section>
     `, // or use templateUrl
   controller: HomeController
