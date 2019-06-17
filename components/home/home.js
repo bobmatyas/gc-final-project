ecb97eb69ff774510a0a79f94ca20600b034e5b0
Eric Medema <eamedema@gmail.com>
2019-06-17 19:05:25 -0400

    fixing conflicts

--- components/home/home.js
+++ components/home/home.js
@@ -10,12 +10,8 @@ angular.module('ColorApp').component('home', {
   template: `
       <section id="home">
       <div class="home-text">
-<<<<<<< HEAD
-         <h1 class="fonts">What color photos are you looking for?</h1>
-=======
          <h1>Find the perfect picture. Find the perfect palette.</h1>
          <h2>All in one place.</h2>
->>>>>>> master
       </div>
   
       <main-search></main-search>
