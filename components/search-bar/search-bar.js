3dc9982c906425cf683878e15eb49a96a4b97d9a
Eric Medema <eamedema@gmail.com>
2019-06-17 00:27:04 -0400

    adding search styling

--- components/search-bar/search-bar.js
+++ components/search-bar/search-bar.js
@@ -47,20 +47,23 @@ function SearchBarController() {
 
 angular.module('ColorApp').component('searchBar', {
   template: `
-        <h2>Search</h2>
-        <input type="text" maxlength="100" ng-model="$ctrl.photoSearch" placeholder="search by keyword" ng-keypress="($event.charCode==13)? $ctrl.getPhotos({queryText: $ctrl.photoSearch, photoCategory: $ctrl.photoCategory, photoOrientation: $ctrl.photoOrientation, selectedColor: $ctrl.selectedColor}) : return" />
-        <select ng-model="$ctrl.photoCategory">
-          <option value="" disabled selected hidden>Please Choose... </option>
-          <option value=""></option>
-          <option ng-repeat="category in $ctrl.photoCategories" value="{{ category }}"> {{ category.charAt(0).toUpperCase()+ category.substr(1).toLowerCase()  }}</option>
-        </select>
-        <select ng-model="$ctrl.photoOrientation"> 
-          <option ng-repeat="orientation in $ctrl.photoOrientations" value="{{ orientation }}"> {{ orientation.charAt(0).toUpperCase()+ orientation.substr(1).toLowerCase()  }}</option>
-        </select>
-        <button class="button-green" ng-click="$ctrl.getPhotos({queryText: $ctrl.photoSearch, photoCategory: $ctrl.photoCategory, photoOrientation: $ctrl.photoOrientation, selectedColor: $ctrl.color,})">
+      <div class="searchContainer">
+        <h2 class="fonts">Search</h2>
+        <input class="searchInput inputText" type="text" maxlength="100" ng-model="$ctrl.photoSearch" placeholder="search by keyword" ng-keypress="($event.charCode==13)? $ctrl.getPhotos({queryText: $ctrl.photoSearch, photoCategory: $ctrl.photoCategory, photoOrientation: $ctrl.photoOrientation, selectedColor: $ctrl.selectedColor}) : return" />
+        <div class="searchInput filters">
+          <select ng-model="$ctrl.photoCategory">
+            <option value="" disabled selected hidden>Please Choose... </option>
+            <option value=""></option>
+            <option ng-repeat="category in $ctrl.photoCategories" value="{{ category }}"> {{ category.charAt(0).toUpperCase()+ category.substr(1).toLowerCase()  }}</option>
+          </select>
+          <select ng-model="$ctrl.photoOrientation"> 
+            <option ng-repeat="orientation in $ctrl.photoOrientations" value="{{ orientation }}"> {{ orientation.charAt(0).toUpperCase()+ orientation.substr(1).toLowerCase()  }}</option>
+          </select>
+        </div>
+        <button class="searchInput button-green" ng-click="$ctrl.getPhotos({queryText: $ctrl.photoSearch, photoCategory: $ctrl.photoCategory, photoOrientation: $ctrl.photoOrientation, selectedColor: $ctrl.color,})">
           Search
         </button>
-       
+      </div>
     `, // or use templateUrl
   controller: SearchBarController,
   bindings: {
