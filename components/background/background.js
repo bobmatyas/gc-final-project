915b887799daf7cc3d5fe591b1bbfb4875f37063
Marion Luxem <marion@scottallencreative.com>
2019-06-20 22:03:49 -0400

    added tagline to banner

--- components/background/background.js
+++ components/background/background.js
@@ -6,6 +6,10 @@ function BackgroundController() {
   angular.module('ColorApp').component('background', {
     template: `
     <div class="backgroundBody">
+    <div class="bg-title">
+        <h1 class="title-pic">Find the perfect picture.</h1> <h1 class="title-pal">Find the perfect palette.</h1>
+        <h2 class="title-all">All in one place.</h2>
+    </div>
     <div class="rowContainer">
         <div class="row1"></div><div class="row2"></div><div class="row3"></div><div class="row4"></div><div class="row2"></div><div class="row1"></div><div class="row2"></div><div class="row5"></div><div class="row3"></div><div class="row4"></div><div class="row2"></div><div class="row5"></div><div class="row3"></div><div class="row2"></div><div class="row4"></div><div class="row1"></div><div class="row2"></div><div class="row1"></div><div class="row4"></div><div class="row2"></div><div class="row5"></div><div class="row1"></div><div class="row2"></div><div class="row1"></div><div class="row4"></div><div class="row2"></div><div class="row2"></div><div class="row1"></div><div class="row4"></div><div class="row2"></div>
     </div>
@@ -18,6 +22,9 @@ function BackgroundController() {
     <div class="rowContainer">
         <div class="row3"></div><div class="row1"></div><div class="row5"></div><div class="row3"></div><div class="row1"></div><div class="row5"></div><div class="row1"></div><div class="row4"></div><div class="row2"></div><div class="row5"></div><div class="row3"></div><div class="row5"></div><div class="row2"></div><div class="row5"></div><div class="row4"></div><div class="row1"></div><div class="row4"></div><div class="row1"></div><div class="row3"></div><div class="row5"></div><div class="row4"></div><div class="row1"></div><div class="row4"></div><div class="row1"></div><div class="row3"></div><div class="row5"></div><div class="row4"></div><div class="row1"></div><div class="row3"></div><div class="row5"></div>
     </div>
-     </div> `,
+</div>
+     
+    
+     `,
     controller: BackgroundController,
   });
